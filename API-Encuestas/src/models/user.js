6dedb9525c3583978efbea10f5e72228d43949ad
NoeAlejandroRodriguezMoto <alxg5516@gmail.com>
2024-03-20 23:06:58 -0600

    Agrega definición de modelo de usuario con campos y ganchos de Sequelize

--- API-Encuestas/src/models/user.js
+++ API-Encuestas/src/models/user.js
@@ -1,8 +1,49 @@
-import { DataTypes, Model } from 'sequelize';
-import { sequelize } from '../database/db.config.js';
+import { DataTypes, Model } from "sequelize";
+import { sequelize } from "../database/db.config.js";
 
 export class User extends Model {}
 
-User.init({
-    
-});
+User.init(
+  {
+    googleId: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    registrationMethod: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    modelName: "user",
+    hooks: {
+      beforeCreate: async (user) => {
+        if (user.password) {
+          const rounds = 10;
+          const salt = await bcrypt.genSalt(rounds);
+          const hash = await bcrypt.hash(user.password, salt);
+          user.password = hash;
+        }
+
+        if (user.googleId) {
+          user.registrationMethod = "google";
+        } else {
+          user.registrationMethod = "local";
+        }
+      },
+    },
+  }
+);
