1a3c3b2da66c1524feeaed46bd42a17628a430cb
NoeAlejandroRodriguezMoto <alxg5516@gmail.com>
2024-03-21 11:47:49 -0600

    Agregada autenticación y sincronización de la base de datos

--- API-Encuestas/src/app/app.server.js
+++ API-Encuestas/src/app/app.server.js
@@ -1,18 +1,21 @@
 import express from 'express';
 import cors from 'cors';
 import morgan from 'morgan';
+import { sequelize } from '../database/db.config.js';
 
 export async function server() {
     try {
         const app = express();
         const PORT = process.env.PORT;
 
+        await sequelize.authenticate();
+        await sequelize.sync({ force: true });
+
         app.use(cors());
         app.use(morgan('dev'));
         app.use(express.json());
         
         app.listen(PORT);
-        console.log(PORT);
     } catch (error) {
         console.error('Error: ', error);
     }
