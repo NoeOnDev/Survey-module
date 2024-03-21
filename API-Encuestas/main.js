6eccda74a6db49df07838c01429ed2b2b8fe0d16
NoeAlejandroRodriguezMoto <alxg5516@gmail.com>
2024-03-20 18:24:36 -0600

    Agregado código para autenticación de Google y modelo de usuario

---
+++ API-Encuestas/main.js
@@ -0,0 +1,73 @@
+import express from "express";
+import morgan from "morgan";
+import cors from "cors";
+import passport from "passport";
+import GoogleStrategy from "passport-google-oauth20";
+import { Sequelize, DataTypes, Model } from "sequelize";
+
+process.loadEnvFile();
+
+const app = express();
+const { PORT, DB_NAME, DB_USER, DB_PASSWORD, DB_HOST, DB_PORT } = process.env;
+
+app.use(cors());
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(passport.initialize());
+
+app.get(
+  "/auth/google",
+  passport.authenticate("google", { scope: ["profile"] })
+);
+
+app.get(
+  "/auth/google/callback",
+  passport.authenticate("google", { failureRedirect: "/login" }),
+  function (req, res) {
+    res.redirect("http://localhost:5173/");
+  }
+);
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
+const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
+  host: DB_HOST,
+  port: DB_PORT,
+  dialect: "postgres",
+  logging: false,
+});
+
+sequelize.authenticate();
+sequelize.sync({ force: true });
+
+class User extends Model {}
+
+User.init(
+  {
+    googleId: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    modelName: "user",
+  }
+);
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: process.env.GOOGLE_CALLBACK_URL,
+    },
+    function (accessToken, refreshToken, profile, cb) {
+      User.findOrCreate({ googleId: profile.id }, function (err, user) {
+        return cb(err, user);
+      });
+    }
+  )
+);
---
+++ API-Encuestas/src/models/user.js
@@ -0,0 +1,8 @@
+import { DataTypes, Model } from 'sequelize';
+import { sequelize } from '../database/db.config.js';
+
+export class User extends Model {}
+
+User.init({
+    
+});
