afef77c3fe92392ec5779fe105eafd196521115a
NoeAlejandroRodriguezMoto <alxg5516@gmail.com>
2024-03-24 19:58:29 -0600

    Actualizar estilos de código de verificación y formulario de código de verificación

--- Front-Encuestas/src/components/template/VerificationCodeForm.jsx
+++ Front-Encuestas/src/components/template/VerificationCodeForm.jsx
@@ -1,16 +1,42 @@
+import React, { useRef } from 'react';
 import styles from "../../assets/styles/verificationCodeStyles.module.css";
 
 function VerificationCodeForm() {
+    const inputs = Array(6).fill(0).map(() => useRef(null));
+
+    const focusNext = (event, i) => {
+        const val = event.target.value;
+        if (!/^\d$/.test(val)) {
+            event.target.value = '';
+            return;
+        }
+
+        if (val.length === 1 && i < inputs.length - 1) {
+            inputs[i + 1].current.focus();
+        }
+    };
+
+    const focusPrev = (event, i) => {
+        if (event.key === 'Backspace' && event.target.value === '' && i > 0) {
+            inputs[i - 1].current.focus();
+        }
+    };
+
     return (
         <div className={styles.container}>
             <form className={styles.form}>
                 <div className={styles.inputFields}>
-                    <input maxLength="1" type="tel" placeholder="" />
-                    <input maxLength="1" type="tel" placeholder="" />
-                    <input maxLength="1" type="tel" placeholder="" />
-                    <input maxLength="1" type="tel" placeholder="" />
-                    <input maxLength="1" type="tel" placeholder="" />
-                    <input maxLength="1" type="tel" placeholder="" />
+                    {inputs.map((inputRef, i) => (
+                        <input
+                            key={i}
+                            ref={inputRef}
+                            maxLength="1"
+                            type="tel"
+                            placeholder=""
+                            onChange={(event) => focusNext(event, i)}
+                            onKeyDown={(event) => focusPrev(event, i)}
+                        />
+                    ))}
                 </div>
             </form>
         </div>
