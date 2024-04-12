6aef3db19875933773d5abd4c3c57c5cb63ee4ea
NoeAlejandroRodriguezMoto <alxg5516@gmail.com>
2024-04-12 16:28:14 -0600

    Creando las funciones startServer, startProject, startProject

--- API-Encuestas/server.js
+++ API-Encuestas/server.js
@@ -11,11 +11,22 @@ const { DB_NAME, DB_USER, DB_PASSWORD , DB_HOST, DB_PORT } = process.env;
 // Database configuration
 export const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
     host: DB_HOST,
-    port: DB_PORT,
+    port: Number(DB_PORT),
     dialect: "postgres",
     logging: false,
 });
-
+// Database connection
+export async function  connectDataBase() {
+    try {
+        await sequelize.authenticate();
+        console.log("Connection has been established successfully.");
+        await  sequelize.sync({ force: false });
+        console.log("All models were synchronized successfully.");
+    } catch (error) {
+        console.error(error);
+    }
+}
+// Server configuration
 export async function startServer() {
     try {
         const PORT = process.env.PORT;
@@ -31,7 +42,6 @@ export async function startServer() {
         app.use(morgan("dev"));
         app.use(cookieParser());
         app.use(passport.initialize());
-        app.use(passport.session());
         app.use(express.urlencoded({ extended: false }));
 
         app.listen(PORT, () => {
@@ -41,6 +51,14 @@ export async function startServer() {
         console.error(error);
     }
 }
+// Start project
+export async function startProject() {
+    try {
+        await connectDataBase();
+        await startServer();
+    } catch (error) {
+        console.error(error);
+    }
+}
 
-startServer();
-
+startProject().catch(error => console.error(error));
