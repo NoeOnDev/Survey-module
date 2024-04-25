e9d54b4e145ee6af60b11798ea26b6487eacc39e
NoeAlejandroRodriguezMoto <alxg5516@gmail.com>
2024-04-24 23:56:14 -0600

    Actualizar manejo de errores de validación en middleware/validationErrorHandler.js y validation/userValidator.js

--- API-Encuestas/src/validation/userValidator.js
+++ API-Encuestas/src/validation/userValidator.js
@@ -1,17 +1,5 @@
 import { check, validationResult } from "express-validator";
-import CustomError from "../helper/customErrorHelper.js";
-
-const handleValidationErrors = (req, res, next) => {
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    return next(
-      new CustomError(400, "Invalid input", "INVALID_INPUT", {
-        errors: errors.array(),
-      })
-    );
-  }
-  next();
-};
+import { handleValidationErrors } from "../middleware/validationErrorHandler.js";
 
 export const validateUser = [
   check("email")
