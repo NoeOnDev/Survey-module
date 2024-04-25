9a457439a8f6e8c200502d697accc3e6447a90eb
NoeAlejandroRodriguezMoto <alxg5516@gmail.com>
2024-04-24 23:45:12 -0600

    Actualizar validación de usuario en userRoutes.js y userValidator.js

--- API-Encuestas/src/routes/userRoutes.js
+++ API-Encuestas/src/routes/userRoutes.js
@@ -1,10 +1,10 @@
 import { Router } from "express";
-import { validateUser } from "../validation/userValidator.js";
+import { validateUser, validateUserVerification } from "../validation/userValidator.js";
 import userController from "../controller/userController.js";
 
 const router = Router();
 
 router.post("/users", validateUser, userController.createUser);
-router.post("/users/verify", userController.verifyUser);
+router.post("/users/verify", validateUserVerification, userController.verifyUser);
 
 export default router;
--- API-Encuestas/src/validation/userValidator.js
+++ API-Encuestas/src/validation/userValidator.js
@@ -1,21 +1,38 @@
 import { check, validationResult } from "express-validator";
 import CustomError from "../helper/customErrorHelper.js";
 
+const handleValidationErrors = (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return next(
+      new CustomError(400, "Invalid input", "INVALID_INPUT", {
+        errors: errors.array(),
+      })
+    );
+  }
+  next();
+};
+
 export const validateUser = [
   check("email")
     .trim()
     .isEmail()
     .withMessage("Must be a valid email")
     .normalizeEmail(),
-  (req, res, next) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return next(
-        new CustomError(400, "Invalid input", "INVALID_INPUT", {
-          errors: errors.array(),
-        })
-      );
-    }
-    next();
-  },
+  handleValidationErrors
+];
+
+export const validateUserVerification = [
+  check("email")
+    .trim()
+    .isEmail()
+    .withMessage("Must be a valid email")
+    .normalizeEmail(),
+  check("code")
+    .trim()
+    .isNumeric()
+    .withMessage("Must be a number")
+    .isLength({ min: 6, max: 6 })
+    .withMessage("Must be a 6 digit code"),
+  handleValidationErrors
 ];
