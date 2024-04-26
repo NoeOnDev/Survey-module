454c034772bf5f15603ec444c5470367dcc2b8c5
NoeAlejandroRodriguezMoto <alxg5516@gmail.com>
2024-04-25 22:40:46 -0600

    Actualizar exportación de variables en database.js

--- API-Encuestas/src/factory/userServiceFactory.js
+++ API-Encuestas/src/factory/userServiceFactory.js
@@ -4,5 +4,10 @@ import codeGeneratorHelper from "../helper/codeGeneratorHelper.js";
 import emailService from "./emailServiceFactory.js";
 import UserService from "../service/userService.js";
 
-const userService = new UserService(User, CustomError, codeGeneratorHelper, emailService);
+const userService = new UserService(
+  User,
+  CustomError,
+  codeGeneratorHelper,
+  emailService
+);
 export default userService;
