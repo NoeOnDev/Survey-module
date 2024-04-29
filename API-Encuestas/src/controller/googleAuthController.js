6ededd32b268e6726a2702612cf9548b1898e5b7
NoeAlejandroRodriguezMoto <alxg5516@gmail.com>
2024-04-29 02:35:54 -0600

    Agregar autenticación de Google en el controlador de autenticación de Google

--- API-Encuestas/src/controller/googleAuthController.js
+++ API-Encuestas/src/controller/googleAuthController.js
@@ -1,35 +1,38 @@
 class GoogleAuthController {
-    constructor(googleAuthService, jwtHelper, cookieHelper) {
-        this.googleAuthService = googleAuthService;
-        this.jwtHelper = jwtHelper;
-        this.cookieHelper = cookieHelper;
+  constructor(googleAuthService, jwtHelper, cookieHelper) {
+    this.googleAuthService = googleAuthService;
+    this.jwtHelper = jwtHelper;
+    this.cookieHelper = cookieHelper;
+  }
+
+  authenticate = (req, res, next) => {
+    try {
+      const authenticate = this.googleAuthService.authenticate();
+      authenticate(req, res, next);
+    } catch (error) {
+      next(error);
     }
+  };
 
-    authenticate = (req, res, next) => {
-        try {
-            const authenticate = this.googleAuthService.authenticate();
-            authenticate(req, res, next);
-        } catch (error) {
-            next(error);
+  callback = (req, res, next) => {
+    try {
+      const callback = this.googleAuthService.callback();
+      callback(req, res, async (error) => {
+        if (error) {
+          next(error);
+        } else {
+          const token = this.jwtHelper.generateToken(
+            req.user.id,
+            req.user.email
+          );
+          this.cookieHelper.setTokenCookie(res, token);
+          res.redirect("http://localhost:5173/home");
         }
-    };
-
-    callback = (req, res, next) => {
-        try {
-            const callback = this.googleAuthService.callback();
-            callback(req, res, async (error) => {
-                if (error) {
-                    next(error);
-                } else {
-                    const token = this.jwtHelper.generateToken(req.user.id, req.user.email);
-                    this.cookieHelper.setTokenCookie(res, token);
-                    res.redirect("http://localhost:5173/home");
-                }
-            });
-        } catch (error) {
-            next(error);
-        }
-    };
+      });
+    } catch (error) {
+      next(error);
+    }
+  };
 }
 
 export default GoogleAuthController;
